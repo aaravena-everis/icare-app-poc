6140728a16834476c1c0b2a9e9dbce10794251b4
Alejandra Aravena Silva - Everis <alejandra.aravena.silva@everis.com>
2018-05-16 15:31:56 -0400

    base icare

--- config/config.js
+++ config/config.js
@@ -7,10 +7,14 @@ config.dbOptions = {
         poolSize: 10,
         reconnectTries: Number.MAX_VALUE
     },
-    user: 'icare-dev',
-    pass: 'icare-dev'
+        //user: 'icare-dev',
+        //pass: 'icare-dev'
+    	user: 'usuario1',
+        pass: 'clave1'
 };
-config.dbEndpoint = 'mongodb://ds053218.mlab.com:53218/heroku_4m0g8nhg'; 
+
+//config.dbEndpoint = 'mongodb://ds053218.mlab.com:53218/heroku_4m0g8nhg';
+config.dbEndpoint = 'mongodb://13.90.142.201:27017/app_qa'; 
 config.web.port = process.env.PORT || 8080;
 config.secret = 'URsltltR453Cr3TP4sSW0Rd';
 module.exports = config;
